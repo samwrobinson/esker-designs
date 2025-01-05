14af507d16b4e61258dcd4d9dcf1902de2a4c063
Sam Robinson <samrobinson@Sams-Mac-mini.home>
2025-01-05 15:47:51 -0500

    trying to optimize swiper

--- src/assets/js/nav.js
+++ src/assets/js/nav.js
@@ -109,24 +109,6 @@ document.addEventListener('DOMContentLoaded', () => {
   });
   
 
-  window.addEventListener('load', () => {
-    const swiper = new Swiper('.swiper-container', {
-        slidesPerView: 'auto',
-        centeredSlides: true,
-        spaceBetween: 30,
-        loop: true,
-        navigation: {
-            nextEl: '.swiper-button-next',
-            prevEl: '.swiper-button-prev',
-        },
-        pagination: {
-            el: '.swiper-pagination',
-            clickable: true,
-        },
-    });
-});
-
-
   
 
   
