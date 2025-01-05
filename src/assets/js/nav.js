f954c27ef1ba3c27352a4aa34b2ff9bc72105810
Sam Robinson <samrobinson@Sams-Mac-mini.home>
2025-01-05 14:48:06 -0500

    try to fix swiper

--- src/assets/js/nav.js
+++ src/assets/js/nav.js
@@ -109,20 +109,23 @@ document.addEventListener('DOMContentLoaded', () => {
   });
   
 
-  const swiper = new Swiper('.swiper-container', {
-    slidesPerView: 'auto',
-    centeredSlides: true,
-    spaceBetween: 30,
-    loop: false,
-    navigation: {
-        nextEl: '.swiper-button-next',
-        prevEl: '.swiper-button-prev',
-    },
-    pagination: {
-        el: '.swiper-pagination',
-        clickable: true,
-    },
+  window.addEventListener('load', () => {
+    const swiper = new Swiper('.swiper-container', {
+        slidesPerView: 'auto',
+        centeredSlides: true,
+        spaceBetween: 30,
+        loop: true,
+        navigation: {
+            nextEl: '.swiper-button-next',
+            prevEl: '.swiper-button-prev',
+        },
+        pagination: {
+            el: '.swiper-pagination',
+            clickable: true,
+        },
+    });
 });
+
 
   
 
